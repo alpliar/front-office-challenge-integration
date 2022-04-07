9b29d29d359a09587a0c4ac9311523a1683e375b
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-04-07 23:54:46 +0200

    feature: init carousel content

--- src/components/DisciplinesCarousel.tsx
+++ src/components/DisciplinesCarousel.tsx
@@ -1,16 +1,58 @@
-import { Empty } from 'antd'
+import { Carousel, Empty } from 'antd'
 import React from 'react'
 import Title from './Title'
+// import { LeftCircleFilled, RightCircleFilled } from '@ant-design/icons'
+
+// const NextArrow: React.FC = () => {
+//   return (
+//     <span style={{ backgroundColor: 'gold' }}>
+//       <RightCircleFilled />
+//     </span>
+//   )
+// }
+// const PrevArrow: React.FC = () => {
+//   return (
+//     <span style={{ backgroundColor: 'gold' }}>
+//       <LeftCircleFilled />
+//     </span>
+//   )
+// }
 
 const DisciplinesCarousel: React.FC = () => {
-  const events = null
+  const events = {}
 
   return (
     <>
       <Title level={2}>Prochaines épreuves</Title>
 
       {!events && <Empty />}
-      {events && <div>events</div>}
+      {events && (
+        <>
+          <Carousel
+            arrows
+            centerMode
+            style={{ display: 'flex', justifyItems: 'space-around' }}
+            // nextArrow={<NextArrow />}
+            // prevArrow={<PrevArrow />}
+          >
+            <div>
+              <h3 style={{ backgroundColor: 'tomato' }}>Item 1</h3>
+            </div>
+            <div>
+              <h3 style={{ backgroundColor: 'teal' }}>Item 2</h3>
+            </div>
+            <div>
+              <h3 style={{ backgroundColor: 'gold' }}>Item 3</h3>
+            </div>
+            <div>
+              <h3 style={{ backgroundColor: 'navy' }}>Item 4</h3>
+            </div>
+            <div>
+              <h3 style={{ backgroundColor: 'violet' }}>Item 5</h3>
+            </div>
+          </Carousel>
+        </>
+      )}
     </>
   )
 }
