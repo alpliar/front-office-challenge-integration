a83b9e6ebdfe8aba2d5d789ed953cff77e97ae85
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-04-07 14:57:30 +0200

    style: main content bg & dimensions

--- src/components/AppMainContainer.tsx
+++ src/components/AppMainContainer.tsx
@@ -3,8 +3,13 @@ import React from 'react'
 import DisciplinesSelect from './DisciplinesSelect'
 
 const AppMainContainer = () => {
+  const contentStyle: React.CSSProperties = {
+    backgroundColor: '#F0F2F5',
+    maxWidth: '96em',
+    margin: 'auto',
+  }
   return (
-    <Content>
+    <Content style={contentStyle}>
       <DisciplinesSelect />
     </Content>
   )
