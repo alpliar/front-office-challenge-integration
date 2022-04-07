8f0c25a39b185bab50d956646f898a26ddc98505
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-04-07 14:57:42 +0200

    style: header styles

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -3,9 +3,16 @@ import React from 'react'
 import Title from './Title'
 
 const AppHeader = () => {
+  const headerStyle: React.CSSProperties = {
+    backgroundColor: '#D4D4D4', // Why the hell does Antd Header component has dark background & text by default ?
+    textAlign: 'center',
+    padding: '1em',
+    height: 'inherit', // Antd Header component has a reduced height by default...
+  }
+
   return (
-    <Header>
-      <Title>JO 2020</Title>
+    <Header style={headerStyle}>
+      <Title noMargin>JO 2020</Title>
     </Header>
   )
 }
