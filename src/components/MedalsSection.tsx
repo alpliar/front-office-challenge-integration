349c63f6cc88de3063059f877af57d719a3aea0d
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-04-08 00:19:05 +0200

    feature: make table sortable

--- src/components/MedalsSection.tsx
+++ src/components/MedalsSection.tsx
@@ -15,21 +15,25 @@ const MedalsSection: React.FC = () => {
       title: 'Or',
       dataIndex: 'gold',
       key: 'gold',
+      sorter: (a: { gold: number }, b: { gold: number }) => a.gold - b.gold,
     },
     {
       title: 'Argent',
       dataIndex: 'silver',
       key: 'silver',
+      sorter: (a: { silver: number }, b: { silver: number }) => a.silver - b.silver,
     },
     {
       title: 'Bronze',
       dataIndex: 'bronze',
       key: 'bronze',
+      sorter: (a: { silver: number }, b: { silver: number }) => a.silver - b.silver,
     },
     {
       title: 'Total',
       dataIndex: 'total',
       key: 'total',
+      sorter: (a: { total: number }, b: { total: number }) => a.total - b.total,
     },
   ]
 
