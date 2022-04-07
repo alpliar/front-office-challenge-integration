23674758da150cd5864f40876b2961870849a8ed
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-04-07 10:56:40 +0200

    refactor: rename header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Header from './components/Header'
+import AppHeader from './components/AppHeader'
 
 function App() {
   return (
     <>
-      <Header />
+      <AppHeader />
     </>
   )
 }
