5d01af7df90ce8f4ec6d5d74f648c03dea89564d
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-04-08 17:55:26 +0200

    style: remove divider between disciplines seletion & carousel

--- src/components/AppMainContainer.tsx
+++ src/components/AppMainContainer.tsx
@@ -16,7 +16,6 @@ const AppMainContainer = () => {
   return (
     <Content style={contentStyle}>
       <DisciplinesSelect />
-      <Divider />
       <DisciplinesCarousel />
       <Divider />
       <MedalsSection />
