95bc6b02d33b9e8d710f6973464ad3f0a10254da
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-04-08 14:24:46 +0200

    feature: implement context for discipline selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AppFooter from './components/AppFooter'
 import AppHeader from './components/AppHeader'
 import AppLayout from './components/AppLayout'
 import AppMainContainer from './components/AppMainContainer'
+import EventsContext, { defaultContext as defaultEvents } from './context/EventsContext'
 
 function App() {
+  const [context, setContext] = useState<string[]>(defaultEvents)
+
   return (
-    <AppLayout>
-      <AppHeader />
-      <AppMainContainer />
-      <AppFooter />
-    </AppLayout>
+    <EventsContext.Provider value={{ context, setContext }}>
+      <AppLayout>
+        <AppHeader />
+        <AppMainContainer />
+        <AppFooter />
+      </AppLayout>
+    </EventsContext.Provider>
   )
 }
 
